8d9da69965dc7c3d06e7757bfb0d17c0502a8970
hamid-murambiwa <machipisajunior@gmail.com>
2021-10-25 17:40:03 +0200

    Add js linters code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const config = require('./webpack.config');
+const config = require('./webpack.config.js');
 
 module.exports = merge(config, {
   mode: 'production',
